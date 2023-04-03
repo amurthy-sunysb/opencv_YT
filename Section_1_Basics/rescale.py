7cc30c42b530110a4205a94014870d5aba2db6cb
Abhishek Murthy <amurthy.sunysb@gmail.com>
2023-04-03 14:55:46 -0400

    recale.py

---
+++ Section_1_Basics/rescale.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+
+img = cv.imread('../Resources/Photos/cat.jpg')
+#cv.imshow('Cat', img)
+
+def rescaleFrame(frame, scale=0.75):
+    # Works for images, video, live video
+    width = int(frame.shape[1] *  scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+resized_image = rescaleFrame(img)
+cv.imshow('Image', resized_image)
+
+def changeRes(width, height):
+    # works only on live video
+    capture.set(3, width)
+    capture.set(4, height)
+
+# Reading videos
+capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame, scale=0.1)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+
+cv.waitKey(0)
